3e01c8e7742878cf4bd57a1868483c8b358b19f9
Andrew Russell <andy.russell.code@gmail.com>
2017-04-11 17:46:41 -0500

    Added comments to all existing parts and removed all(?) traces of 'art' from main.py.  Form seems to work correctly including error message.  No list of posts appearing

--- main.py
+++ main.py
@@ -18,49 +18,61 @@ import webapp2
 import os
 import jinja2
 
-from google.appengine.ext import db
+from google.appengine.ext import db     #import database funtions
 
+#Link template directory and autoescape
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape = True)
+autoescape = True)
 
 class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+#base handler that others inherit from.
+    def write(self, *arguments, **keywords):        #shortens the .write method for use elsewhere
+        self.response.write(*arguments, **keywords)
 
-    def render_str(self, template, **params):
+    def render_str(self, template, **parameters):   #builds a string to render
         t = jinja_env.get_template(template)
-        return t.render(params)
+        return t.render(parameters)
 
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
+    def render(self, template, **keywords):         #shortens the render method for use elsewhere
+        self.write(self.render_str(template, **keywords))
 
-class Art(db.Model):
+class Post(db.Model):
+#build database where each instance is an entity is a blog post with 3 properties
     title = db.StringProperty(required = True)
-    art = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+    post_body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)  #Print this with posts?
 
-class MainPage(Handler):
-    def render_front(self, title="", art="", error=""):
-        arts = db.GqlQuery("SELECT * FROM Art "
+class MainPage(Handler):            #renders main page upon initial get request
+    def render_front(self, title="", post_body="", error=""):
+        posts = db.GqlQuery("SELECT * FROM Post "
                             "ORDER BY created DESC ")
+    #retrieves entity and assigns to "posts"
 
-        self.render("front.html", title=title, art=art, error=error, arts=arts)
+        self.render("front.html", title=title, post_body=post_body, error=error)
+        #renders front page using .render, info from 'posts' and front template
 
     def get(self):
         self.render_front()
+        #get request calls render_front above
 
     def post(self):
         title = self.request.get("title")
-        art = self.request.get("art")
+        post_body = self.request.get("post_body")
+    #retrieve title and post_body from ??????
 
-        if title and art:
-            a = Art(title = title, art = art)
+        if title and post_body:
+            a = Post(title = title, post_body = post_body)
             a.put()
+        #if title and post_body are present, update db
 
             self.redirect("/")
-        else:
-            error = "we need both a title and some artwork!"
-            self.render_front(title, art, error)
+        #rerender the page
 
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
+        else:
+            error = "We need both a title and a post!"
+            self.render_front(title, post_body, error)
+        #if both not present then rerender the page with input & error message
+
+app = webapp2.WSGIApplication([('/', MainPage)
+], debug=True)
